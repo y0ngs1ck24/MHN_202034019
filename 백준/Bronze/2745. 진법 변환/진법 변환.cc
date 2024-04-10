65954104d4bb4f2df03fcbe6c87f39013e162d61
y0ngs1ck24 <144236201+y0ngs1ck24@users.noreply.github.com>
2024-04-10 19:44:07 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -1,35 +1,36 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-// 거듭제곱 함수 구현
-int power(int base, int exponent) {
-    int result = 1;
-    for (int i = 0; i < exponent; ++i) {
-        result *= base;
-    }
-    return result;
+int v(int byun, int sibal)
+{
+	int a = 1;
+	for (int i = 0; i < sibal; i++)
+	{
+		a *= byun;
+	}
+	return a;
 }
 
-int main() {
-    string n;
-    int b;
-    cin >> n >> b;
-
-    int sum = 0;
-    int length = n.length();
-
-    // 문자열을 뒤에서부터 순회하여 각 자릿수를 10진수로 변환하여 합산
-    for (int i = length - 1; i >= 0; i--) {
-        int digit;
-        if (isdigit(n[i])) {
-            digit = n[i] - '0';
-        } else {
-            digit = n[i] - 'A' + 10;
-        }
-        sum += digit * power(b, length - 1 - i); // 현재 자릿수에 해당하는 진수의 거듭제곱을 곱하여 더함
-    }
-
-    cout << sum;
-    return 0;
+int main()
+{
+	string n;
+	int b;
+	cin >> n >> b;
+	int answer = 0;
+	int sum;
+	for (int i = 0; i < n.length(); i++)
+	{
+		if (n[i] >= 65 && n[i] <= 90)
+		{
+			n[i] = n[i] - 'A' + 10;
+		}
+		else
+		{
+			n[i] = n[i] - '0';
+		}
+		sum = n[i] * v(b, n.length() - i - 1);
+		answer += sum;
+	}
+	cout << answer;
 }
+
